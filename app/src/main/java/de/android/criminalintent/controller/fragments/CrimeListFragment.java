017c13b55e227e74cae19e264290a04866021204
AIRAT1 <ayrat1@mail.ru>
2017-06-26 17:06:44 +0300

    add CrimeHolder.OnClickListener

--- app/src/main/java/de/android/criminalintent/controller/fragments/CrimeListFragment.java
+++ app/src/main/java/de/android/criminalintent/controller/fragments/CrimeListFragment.java
@@ -8,15 +8,19 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 import de.android.criminalintent.R;
 import de.android.criminalintent.model.Crime;
+import de.android.criminalintent.model.CrimeLab;
 
 public class CrimeListFragment extends Fragment{
     private RecyclerView crimeRecyclerView;
+    private CrimeAdapter adapter;
 
     @Nullable
     @Override
@@ -24,15 +28,40 @@ public class CrimeListFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
         crimeRecyclerView = (RecyclerView)view.findViewById(R.id.crime_recycler_view);
         crimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        updateUI();
         return view;
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder {
-        public TextView titleTextView;
+    private void updateUI() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+        adapter = new CrimeAdapter(crimes);
+        crimeRecyclerView.setAdapter(adapter);
+    }
+
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView titleTextView, dateTextView;
+        private CheckBox solvedCheckBox;
+        private Crime crime;
 
         public CrimeHolder(View itemView) {
             super(itemView);
-            titleTextView = (TextView)itemView;
+            itemView.setOnClickListener(this);
+            titleTextView = (TextView)itemView.findViewById(R.id.list_item_crime_title_text_view);
+            dateTextView = (TextView)itemView.findViewById(R.id.list_item_crime_date_text_view);
+            solvedCheckBox = (CheckBox)itemView.findViewById(R.id.list_item_crime_solved_check_box);
+        }
+
+        public void bindCrime(Crime crime) {
+            this.crime = crime;
+            titleTextView.setText(crime.getTitle());
+            dateTextView.setText(crime.getDate().toString());
+            solvedCheckBox.setChecked(crime.isSolved());
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getActivity(), crime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -46,14 +75,14 @@ public class CrimeListFragment extends Fragment{
         @Override
         public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
-            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            View view = layoutInflater.inflate(R.layout.list_item_crime, parent, false);
             return new CrimeHolder(view);
         }
 
         @Override
         public void onBindViewHolder(CrimeHolder holder, int position) {
             Crime crime = crimes.get(position);
-            holder.titleTextView.setText(crime.getTitle());
+            holder.bindCrime(crime);
         }
 
         @Override
